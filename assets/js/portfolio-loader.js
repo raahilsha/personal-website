71a13c4c540bb9e8ec7b072ee625e78ef84d7ff4
raahilsha <raahilsha@gmail.com>
2022-05-19 11:07:06 -0400

    Make portfolio loader code more scalable, redesign portfolio popup modal

--- assets/js/portfolio-loader.js
+++ assets/js/portfolio-loader.js
@@ -1,9 +1,24 @@
-function loadPortfolio1()
-{
-	// Khalid helped here
-	var images = {'img1':'assets/img/portfolio/1/img1.jpg','img2':'assets/img/portfolio/1/img2.jpg','img3':'assets/img/portfolio/1/img3.jpg'};
-	$.each(images, function(){
-	    $('#images-here').append('<img src="' + this + '" />'); 
-	});
-}
+var portfolioData = {
+  "NiagaraFalls": {
+    "Title": "Niagara Falls",
+    "Description": "A post-graduation field trip with my family",
+    "Images": [
+      "assets/img/portfolio/1/img1.jpg",
+      "assets/img/portfolio/1/img2.jpg",
+      "assets/img/portfolio/1/img3.jpg"
+    ]
+  }
+};
 
+function loadPortfolio(portName)
+{
+	if (portName in portfolioData)
+	{
+		$('#portfolio-image-holder').empty();
+		$('#portfolio-page-name').text(portfolioData[portName]["Title"]);
+		$('#portfolio-page-description').text(portfolioData[portName]["Description"]);
+		$.each(portfolioData[portName]["Images"], function(){
+	    	$('#portfolio-image-holder').append('<img class="mb-4" src="' + this + '" />'); 
+		});
+	}
+}
